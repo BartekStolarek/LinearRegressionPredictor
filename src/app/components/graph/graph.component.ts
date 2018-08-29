5a7b099ff50162c500c2c83408d04a8d874d4dde
BartekStolarek <bartek.stolarek@gmail.com>
2018-08-29 15:35:07 +0200

    Small Graph changes

--- src/app/components/graph/graph.component.ts
+++ src/app/components/graph/graph.component.ts
@@ -18,8 +18,8 @@ export class GraphComponent {
     this.loaded = false;
 
     let margin = {top: 50, right: 50, bottom: 50, left: 50}
-    , width = window.innerWidth/1.75 - margin.left - margin.right
-    , height = window.innerHeight/1.75 - margin.top - margin.bottom;
+    , width = window.innerWidth/1.5 - margin.left - margin.right
+    , height = window.innerHeight/1.5 - margin.top - margin.bottom;
 
     // The number of datapoints
     let n = this.data.length;
